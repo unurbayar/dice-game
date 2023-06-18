3609921b4da4a9b70d363b518bdea1870b16e105
unurbayar <steameunuruu25@gmail.com>
2023-06-18 12:02:20 +0800

    querySelector bolon getElementById-oor DOM-oos element songood Math.random()-aas avsan utga aa DOM luu oruulasan

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var scores = [0, 0];
 var roundScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө..
 
-var dice = Math.floor(Math.random() * 6) + 1;
 /* <div class="player-score" id="score-0">43</div> */
 // window.document.querySelector("#score-0").textContent = dice;
 
@@ -15,12 +14,19 @@ var dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector("#score-1").textContent = dice;
 
 // <img src="dice-5.png" alt="Dice" class="dice"></img>
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
 //Програм эхлэхэд бэлтгий
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-console.log("шоо ингэж буулаа: " + dice);
+//<button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
