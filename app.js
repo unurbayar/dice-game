7a08fd75d9276011de5e51fe60b6c9d755ce1184
unurbayar <steameunuruu25@gmail.com>
2023-06-19 19:33:49 +0800

    toglogchiin eeljiig soliod, ulaan temdeglex, roundScore teglex uildluudiig hiisen.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглий.
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 
@@ -24,9 +24,39 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 
 //<button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
-
+// шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 дундаас санамсаргүй тоо гаргана.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // шооны зургыг веб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+  // буусан санамсаргүй тоонд харгалзах шооны зургыг веб дээр дугаараар нь гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+  // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн  оноог  нэмэгдүүлнэ.
+  if (diceNumber != 1) {
+    // Буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийн оноог нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // DOM дээр идэвхтэй тоглогчийг улаан цэгийг устгаж өгнө.
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+
+    // Идэвхтэй тоглогчийн цуглуулсан оноог 0 болгож өгнө.
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // Санах ой дээр хадгаладсан өмнөх тоглогчийн оноог 0 болгож өгнө.
+    roundScore = null;
+    // 1 буусан тул тоглогчийн ээлжийг сольж өгнө.
+    // Хэрвээ идэвхтэй тоглогч нь 0 бол 1 болго.
+    // Хэрвээ идэвхтэй тоглогч нь 1 бол 0 болго.
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // DOM дээр идэвхтэй тоглогчийг улаан цэгээр хэлж өгнө .
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+    // Шоог алга болгож өгнө.
+    diceDom.style.display = "none";
+  }
 });
